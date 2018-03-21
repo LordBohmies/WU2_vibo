c44b9f4f5f8975cd23d17c61164f194372427ab5
LordBohmies <victor.bohmies@gmail.com>
2018-03-21 11:59:07 +0100

    Update 2018-03-21
    * Added task.
    ** Help files for the task.

--- index.php
+++ index.php
@@ -5,17 +5,19 @@ require ('resources/functions/functions.php');
 
 //Create function for the Subpages
 /*
-- Start
-+- Läsa inlägg som News
+- Home - Start
++- Läsa inlägg som Artiklar
 +- Kommentera på inlägg. (Krävs inlogg)
 +- Gilla inläggen.
-- About
-- Posts (Syns bara för inloggade)
-+- Man skall kunna posta inlägg.
-+- Redigera inlägg
-- Profil
+- About - Profil
 +- Redigera information.
 ++- Namn, Bild, Civilstatus osv.
+- Portfolio
++- En Portfolio för alla användare/företaget.
+- Post - Skapa inlägg
++- (Syns bara för inloggade)
++- Man skall kunna posta inlägg.
++- Redigera inlägg
 */
 if (){
 
