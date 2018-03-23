ac4574ca7356e968d18a25b50ffef08a2cbcef2d
LordBohmies <victor.bohmies@gmail.com>
2018-03-23 12:05:10 +0100

    hotfix

--- tasks/part_2/index.php
+++ tasks/part_2/index.php
@@ -1,6 +1,1 @@
-Öppna PHP
 
-I Pseudokod skall ni mellan start och slut-tag för php beskriva struktur för webbsidan i sig.
-Det här kommer bland annat ligga till grund för hur er nav kommer att se ut.
-
-Stäng PHP
